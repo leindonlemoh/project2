53a9f6a3be8acb359c50af6302f1bfa1fa36a13e
joan logronio <joan.mlogronio@gmail.com>
2022-08-24 10:33:51 +0800

    git commit -m "comment"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import About from './components/About/About';
+import Signup from './Signup/Signup';
+import Footer from './Footer/Footer';
 
 const App = () => {
   return (
     <div>
         <Navbar />
+        <About/>
+        <Signup/>
+        <Footer/>
     </div>
   )
 }
 
-export default App
+export default App;
