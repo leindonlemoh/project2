4b4b346771c71e30df4b5ea7b95d59cdaab3f9b9
leindon Reimoh Dilan <leindonreimohdilan@yahoo.com>
2022-08-24 13:38:54 +0800

    arrangement compents in app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import './App.css';
-import About from './components/About/About';
-import Footer from './Footer/Footer';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import About from "./components/About/About";
+import Footer from "./Footer/Footer";
 import Banner from "./components/Banner/Banner";
 import Team from "./components/Team/Team";
 
 const App = () => {
   return (
     <div>
-        <Navbar />
-        <About/>
-        <Footer/>
-        <Banner />
-        <Team />
+      <Navbar />
+      <Banner />
+      <Team />
+      <About />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
