673f77b8a8d03d84251876ce243e31a9bf574e26
joan logronio <joan.mlogronio@gmail.com>
2022-08-24 10:38:47 +0800

    git commit -m "remove signup"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import About from './components/About/About';
-import Signup from './Signup/Signup';
 import Footer from './Footer/Footer';
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
     <div>
         <Navbar />
         <About/>
-        <Signup/>
         <Footer/>
     </div>
   )
