c04f3408f69a1bdb7c9210cbec300cb6cfe5daad
Wendell <vvendell0304@gmail.com>
2022-08-24 13:29:15 +0800

    added Course Component

---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Courses.css';
+import './CourseCard/CourseCard';
+import CourseCard from './CourseCard/CourseCard';
+
+const Courses = () => {
+  return (
+    <div className="courses flex" id="courses">
+        <h2 className="courses__heading">
+            What We Offer
+        </h2>
+        <section className="courses__container grid">
+            <CourseCard 
+                heading="Level-Up Your Design Skills"
+                description="The Design Career track is here to help you master all the essential skills clients and agency are looking to hire."
+                course="Learn Design"
+            />
+
+            <CourseCard 
+                heading="Start Your Dream Business"
+                description="The Entrepreneur track has everything you need to get your creative business up and running and skyrocket your career."
+                course="Learn Business"
+            />
+
+            <CourseCard 
+                heading="Always Be On The Trend"
+                description="The Personal Development track is here to help you learn the right discipline and mindset in order to become the best version of yourself."
+                course="Learn Technology"
+            />
+        </section>
+    </div>
+  );
+};
+
+export default Courses;
